d9b4e18819e17fcfe52b3eb6be76d0d871121592
Yuya <140081005+oikvpqya@users.noreply.github.com>
2024-06-05 15:56:51 +0900

    refactor: Clean up code

--- app/src/desktopMain/kotlin/com/alexjlockwood/twentyfortyeight/ui/BackHandler.desktop.kt
+++ app/src/desktopMain/kotlin/com/alexjlockwood/twentyfortyeight/ui/BackHandler.desktop.kt
@@ -3,4 +3,4 @@ package com.alexjlockwood.twentyfortyeight.ui
 import androidx.compose.runtime.Composable
 
 @Composable
-actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) = Unit
+actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) {}
