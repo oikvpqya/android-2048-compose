db408a9b5f42e3750f2d1668938ae10997d8531c
Yuya <140081005+oikvpqya@users.noreply.github.com>
2024-09-30 14:49:39 +0900

    refactor: Add DefaultGameRepository.

--- app/src/commonMain/kotlin/com/alexjlockwood/twentyfortyeight/repository/GameRepository.kt
+++ app/src/commonMain/kotlin/com/alexjlockwood/twentyfortyeight/repository/GameRepository.kt
@@ -7,15 +7,22 @@ import io.github.xxfast.kstore.KStore
 const val USER_DATA_FILE_NAME = "user_data.json"
 
 /**
- * Repository class that persists the current 2048 game to KStore.
+ * Repository class that persists the current 2048 game.
  */
-class GameRepository(private val store: KStore<UserData>) {
+interface GameRepository {
 
-    suspend fun fetch(): UserData {
+    suspend fun fetch(): UserData
+
+    suspend fun update(grid: List<List<Tile?>>, currentScore: Int, bestScore: Int)
+}
+
+class DefaultGameRepository(private val store: KStore<UserData>) : GameRepository {
+
+    override suspend fun fetch(): UserData {
         return store.get() ?: UserData.EMPTY_USER_DATA
     }
 
-    suspend fun update(grid: List<List<Tile?>>, currentScore: Int, bestScore: Int) {
+    override suspend fun update(grid: List<List<Tile?>>, currentScore: Int, bestScore: Int) {
         store.set(UserData(grid, currentScore, bestScore))
     }
 }
--- app/src/wasmJsMain/kotlin/Main.kt
+++ app/src/wasmJsMain/kotlin/Main.kt
@@ -2,7 +2,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.alexjlockwood.twentyfortyeight.App
 import com.alexjlockwood.twentyfortyeight.domain.UserData
-import com.alexjlockwood.twentyfortyeight.repository.GameRepository
+import com.alexjlockwood.twentyfortyeight.repository.DefaultGameRepository
 import com.alexjlockwood.twentyfortyeight.repository.USER_DATA_FILE_NAME
 import io.github.xxfast.kstore.storage.storeOf
 
@@ -11,7 +11,7 @@ fun main() {
     val store = storeOf(key = USER_DATA_FILE_NAME, default = UserData.EMPTY_USER_DATA)
     CanvasBasedWindow(canvasElementId = "ComposeTarget") {
         App(
-            repository = GameRepository(store),
+            repository = DefaultGameRepository(store),
         )
     }
 }
