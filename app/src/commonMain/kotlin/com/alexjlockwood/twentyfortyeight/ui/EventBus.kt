ddc9981028dcef42d315e24e865c3316a7d9346f
Yuya <140081005+oikvpqya@users.noreply.github.com>
2025-03-28 19:03:48 +0900

    refactor: Change public EventBus.eventFlow to SharedFlow.

--- app/src/commonMain/kotlin/com/alexjlockwood/twentyfortyeight/ui/EventBus.kt
+++ app/src/commonMain/kotlin/com/alexjlockwood/twentyfortyeight/ui/EventBus.kt
@@ -3,14 +3,26 @@ package com.alexjlockwood.twentyfortyeight.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
-class EventBus<EVENT> {
+interface EventBus<EVENT> {
 
-    val eventFlow = MutableSharedFlow<EVENT>(extraBufferCapacity = 20)
-    fun produceEvent(event: EVENT) = eventFlow.tryEmit(event)
+    val eventFlow: SharedFlow<EVENT>
+    fun produceEvent(event: EVENT)
+}
+
+class DefaultEventBus<EVENT> : EventBus<EVENT> {
+
+    private val mutableEventFlow = MutableSharedFlow<EVENT>(extraBufferCapacity = 20)
+    override val eventFlow = mutableEventFlow.asSharedFlow()
+
+    override fun produceEvent(event: EVENT) {
+        mutableEventFlow.tryEmit(event)
+    }
 }
 
 @Composable
 fun <EVENT> rememberEventBus(): EventBus<EVENT> {
-    return remember { EventBus() }
+    return remember { DefaultEventBus() }
 }
